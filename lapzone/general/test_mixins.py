acdf49d582c25a94e36c3432a74ac2e535395359
Nikita <nikita.gubarev31@gmail.com>
2023-03-15 21:48:58 +0200

    TEST: Create test mixin for testing abstract ModelWithFKToProduct

--- lapzone/general/test_mixins.py
+++ lapzone/general/test_mixins.py
@@ -1,10 +1,12 @@
-from django.db.models import Model
+from django.db import models
+
+from shop.models import Product
 
 
 class ModelMetaOptionsTestMixin:
     """Mixin for testing the base meta options of models."""
 
-    model: Model
+    model: models.Model
     verbose_name: str
     verbose_name_plural: str
     ordering: list[str]
@@ -34,12 +36,17 @@ class ModelWithNameTestMixin:
     """Mixin for testing the 'name' field parameters
     for models that inherited from abstract ModelWithNameTest."""
 
-    model: Model
+    model: models.Model
     # Default parameter values.
     name_blank = False
     name_unique = True
     name_max_length = 100
     name_verbose_name = "Name"
+
+    def test_model_string_representation(self):
+        """Test the model string representation by __str__."""
+        obj = self.model.objects.get(id=1)
+        self.assertEqual(str(obj), obj.name)
 
     def test_name_verbose_name(self):
         """
@@ -84,6 +91,13 @@ class ModelWithNameAndSlugTestMixin(ModelWithNameTestMixin):
     slug_unique = True
     slug_max_length = 100
     slug_verbose_name = "Slug"
+
+    expected_slug: str
+    
+    def test_slug_generating(self):
+        """Test that the slug was generated correctly"""
+        obj: models.Model = self.model.objects.get(id=1)
+        self.assertEqual(obj.slug, self.expected_slug)
 
     def test_slug_verbose_name(self):
         """
@@ -136,7 +150,7 @@ class ModelWithDescriptionTestMixin:
             self.description_verbose_name,
         )
 
-    def test_slug_blank(self):
+    def test_description_blank(self):
         """
         Test that the description field's blank is equal to the description_blank attribute.
         """
@@ -207,4 +221,41 @@ class ModelWithCreatedDateTimeTestMixin:
         self.assertEqual(
             self.model._meta.get_field("created").auto_now_add,
             self.created_auto_now_add,
+        )
+
+
+class ModelWithFKToProductTestMixin:
+    """Mixin for testing the 'product' field parameters
+    for models that inherited from abstract ModelWithFKToProduct."""
+
+    # Default parameter values.
+    product_related_model = Product
+    # product_on_delete = models.CASCADE
+    product_verbose_name = "For product"
+
+    def test_product_verbose_name(self):
+        """
+        Test that the product field's verbose name is equal to the product_verbose_name attribute.
+        """
+        self.assertEqual(
+            self.model._meta.get_field("product").verbose_name,
+            self.product_verbose_name,
+        )
+
+    # def test_product_on_delete(self):
+    #     """
+    #     Test that the product field's on_delete is equal to the product_on_delete attribute.
+    #     """
+    #     self.assertEqual(
+    #         self.model._meta.get_field("product").on_delete,
+    #         self.product_on_delete,
+    #     )
+
+    def test_product_related_model(self):
+        """
+        Test that the product field's related model is equal to the product_related_model attribute.
+        """
+        self.assertEqual(
+            self.model._meta.get_field("product").related_model,
+            self.product_related_model,
         )
