4998911c5de903c97acaf7099b421cc71caeb6fc
Nikita <nikita.gubarev31@gmail.com>
2023-03-16 15:59:42 +0200

    SETTINGS: Add 'ckeditor' to the INSTALLED_APPS
    Add 'ckeditor_uploader' too for using widget with file upload

--- lapzone/lapzone/settings.py
+++ lapzone/lapzone/settings.py
@@ -23,6 +23,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third-party apps
+    "ckeditor",
+    "ckeditor_uploader",
     # My apps
     "shop.apps.ShopConfig",
 ]
