479e569c72bfc246363f0ff2ab2795bc1950162c
Nikita <nikita.gubarev31@gmail.com>
2023-03-19 20:14:32 +0200

    FEAT: Create the HomeView for the "Shop" app

--- lapzone/shop/views.py
+++ lapzone/shop/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from typing import Any
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+from . import services
+from general.views import BaseView
+
+
+class HomeView(BaseView, TemplateView):
+    template_name = "shop/home.html"
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["brands"] = services.get_all_brands()
+        context["categories"] = services.get_all_categories()
+        context["carousel_images"] = services.get_all_carousel_images()
+        return context
