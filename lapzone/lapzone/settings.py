49c472da799c4198b1ae51e28361037bc82007da
Nikita <nikita.gubarev31@gmail.com>
2023-04-01 23:10:11 +0300

    SETTINGS: Add 'recaptcha3' in the INSTALLED_APPS
    Add some settings for 'recaptcha3' (public key, private key)

--- lapzone/lapzone/settings.py
+++ lapzone/lapzone/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     # Third-party apps
     "ckeditor",
     "ckeditor_uploader",
+    "snowpenguin.django.recaptcha3",
     # My apps
     "shop.apps.ShopConfig",
 ]
@@ -117,6 +118,11 @@ EMAIL_USE_SSL = False
 EMAIL_HOST_USER = str(os.getenv("EMAIL_HOST_USER"))
 EMAIL_HOST_PASSWORD = str(os.getenv("EMAIL_HOST_PASSWORD"))
 EMAIL_BACKEND = str(os.getenv("EMAIL_BACKEND"))
+
+RECAPTCHA_PUBLIC_KEY = str(os.getenv("RECAPTCHA_PUBLIC_KEY"))
+RECAPTCHA_PRIVATE_KEY = str(os.getenv("RECAPTCHA_PRIVATE_KEY"))
+RECAPTCHA_DEFAULT_ACTION = "generic"
+RECAPTCHA_SCORE_THRESHOLD = 0.5
 
 LOGGING = {
     "version": 1,
