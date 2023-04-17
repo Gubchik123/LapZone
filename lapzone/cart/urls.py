44c0ad192575a4c0483486009e4fa863eb2d9856
Nikita <nikita.gubarev31@gmail.com>
2023-04-17 21:53:14 +0300

    FIX: Fix bug with the "Cart" app urlpatterns
    Add calling as_view() method (forgot before)

--- lapzone/cart/urls.py
+++ lapzone/cart/urls.py
@@ -5,11 +5,15 @@ from . import views
 
 app_name = "cart"
 urlpatterns = [
-    path("cart/", views.CartDetailView, name="detail"),
-    path("product/<slug:slug>/add-to-cart/", views.CartAddView, name="add"),
+    path("cart/", views.CartDetailView.as_view(), name="detail"),
+    path(
+        "product/<slug:slug>/add-to-cart/",
+        views.CartAddView.as_view(),
+        name="add",
+    ),
     path(
         "product/<slug:slug>/remove-from-cart/",
-        views.CartRemoveView,
+        views.CartRemoveView.as_view(),
         name="remove",
     ),
 ]
