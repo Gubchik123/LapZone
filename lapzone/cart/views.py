7f7851a9297f262df2135f871069ac9619c93a70
Nikita <nikita.gubarev31@gmail.com>
2023-04-17 21:39:45 +0300

    FEAT: Create a CartDetailView
    for displaying the contents of the user's shopping cart
    Create url for this view: "/cart/"

--- lapzone/cart/views.py
+++ lapzone/cart/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from typing import Any
 
-# Create your views here.
+from django.views import generic
+
+from general.views import BaseView
+from .cart import Cart
+
+
+class CartDetailView(BaseView, generic.TemplateView):
+    """View for displaying the contents of the user's shopping cart."""
+
+    template_name = "cart/detail.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        """Adds the user's cart to the template context data and returns it."""
+        context = super().get_context_data(**kwargs)
+        context["cart"] = Cart(self.request)
+        return context
