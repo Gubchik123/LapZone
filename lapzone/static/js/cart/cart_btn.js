20c1a2de94fcf71733b7c1f07c52b59c43edddaa
Nikita <nikita.gubarev31@gmail.com>
2023-04-19 20:59:08 +0300

    FEAT: Remake a little of the cart_btn.js
    Add function to replace cart btn with link to cart detail page after sending POST fetch request
    Add function to increase cart badge count of products in user's cart after success POST request

--- lapzone/static/js/cart/cart_btn.js
+++ lapzone/static/js/cart/cart_btn.js
@@ -1,29 +1,47 @@
-document.querySelectorAll(".cart").forEach((cart_btn) => {
-    cart_btn.addEventListener("click", function () {
-        fetch("/cart/add/", {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-                "X-CSRFToken": csrf_token, // Template variable
-            },
-            body: JSON.stringify({
-                product_id: cart_btn.attributes["data-product_id"].nodeValue,
-            }),
-        })
-            .then((response) => response.text())
-            .then((response_text) => {
-                if (
-                    response_text ==
-                    "Product has successfully added to your cart."
-                )
-                    cart_btn.firstElementChild.name = "cart";
+function replace_cart_btn_with_link_to_cart_detail_page(cart_btn) {
+	const link = document.createElement("a");
+	link.href = "/cart/";
+	link.classList = "btn btn-success ms-2";
+	link.innerHTML = "<ion-icon name='cart'></ion-icon>";
 
-                alert(response_text);
-            })
-            .catch((error) => {
-                console.log(error);
-                alert("There was an error! Try again later.");
-            });
-    });
+    cart_btn.parentNode.replaceChild(link, cart_btn);
+}
+
+function increase_cart_badge_count_of_products_in_cart() {
+	const header_cart_badge = document.querySelector(
+		"header ion-icon[name='cart']"
+	).nextElementSibling;
+	header_cart_badge.textContent = +header_cart_badge.textContent + 1;
+}
+
+document.querySelectorAll(".cart").forEach((cart_btn) => {
+	cart_btn.addEventListener("click", function () {
+		fetch("/cart/add/", {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+				"X-CSRFToken": csrf_token, // Template variable
+			},
+			body: JSON.stringify({
+				product_id: cart_btn.attributes["data-product_id"].nodeValue,
+			}),
+		})
+			.then((response) => response.text())
+			.then((response_text) => {
+				if (
+					response_text ==
+					"Product has successfully added to your cart."
+				) {
+					replace_cart_btn_with_link_to_cart_detail_page(cart_btn);
+					increase_cart_badge_count_of_products_in_cart();
+				}
+
+				alert(response_text);
+			})
+			.catch((error) => {
+				console.log(error);
+				alert("There was an error! Try again later.");
+			});
+	});
 });
