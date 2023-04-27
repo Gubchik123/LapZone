ed083c6b38c3f956bc34b45d8fd8114a82115903
Nikita <nikita.gubarev31@gmail.com>
2023-04-27 22:12:06 +0300

    FEAT: Add an url for the OrderDeleteView: "<uuid:pk>/delete/"

--- lapzone/order/urls.py
+++ lapzone/order/urls.py
@@ -6,4 +6,5 @@ from . import views
 app_name = "order"
 urlpatterns = [
     path("<uuid:pk>/", views.OrderDetailView.as_view(), name="detail"),
+    path("<uuid:pk>/delete/", views.OrderDeleteView.as_view(), name="delete"),
 ]
