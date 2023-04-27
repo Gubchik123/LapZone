86e3c66595c1a332abfe9ef22968a08fd0cf25cc
Nikita <nikita.gubarev31@gmail.com>
2023-04-27 22:08:06 +0300

    FEAT: Create an OrderDetailView for the order detail page
    in cart/views.py

--- lapzone/order/views.py
+++ lapzone/order/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.views import generic
+from django.db.models import QuerySet
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from . import services
+from .models import Order
+from general.views import BaseView
+
+
+class OrderDetailView(BaseView, LoginRequiredMixin, generic.DetailView):
+    """View for the order detail page."""
+
+    model = Order
+    template_name = "order/detail.html"
+
+    def get_queryset(self):
+        """Returns a queryset of orders that belong to the current user."""
+        return super().get_queryset().filter(user=self.request.user)
+
+    def get_object(self, queryset: QuerySet[Order] | None = None) -> Order:
+        """
+        Returns a user order from the given queryset by the 'pk' URL parameter.
+        """
+        return services.get_user_order_from_(
+            queryset if queryset is not None else self.get_queryset(),
+            self.kwargs["pk"],
+        )
