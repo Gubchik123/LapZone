bb8c20a9bccaef7ded2733af7156c7f41f5eda6d
Nikita <nikita.gubarev31@gmail.com>
2023-04-29 19:12:08 +0300

    FEAT: Add an url for the OrderListView: "list/"

--- lapzone/order/urls.py
+++ lapzone/order/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 app_name = "order"
 urlpatterns = [
+    path("list/", views.OrderListView.as_view(), name="list"),
     path("<uuid:pk>/", views.OrderDetailView.as_view(), name="detail"),
     path("<uuid:pk>/delete/", views.OrderDeleteView.as_view(), name="delete"),
 ]
