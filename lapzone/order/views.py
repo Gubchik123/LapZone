29c8b7549229bee583ca06673327b300f9501b23
Nikita <nikita.gubarev31@gmail.com>
2023-04-30 21:35:04 +0300

    FEAT: Add an OrderCreateFormView for the checkout page

--- lapzone/order/views.py
+++ lapzone/order/views.py
@@ -7,7 +7,38 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from . import services
 from .models import Order
+from .forms import OrderCreateForm
 from general.views import BaseView
+
+
+class OrderCreateFormView(BaseView, generic.FormView):
+    """View for the checkout page."""
+
+    form_class = OrderCreateForm
+    template_name = "order/order_create.html"
+
+    def get_initial(self):
+        """
+        Return the initial data to use for the OrderCreateForm.
+        Checks if the user is authenticated and adds the user data.
+        """
+        if self.request.user.is_authenticated:
+            return {
+                "email": self.request.user.email,
+                "first_name": self.request.user.first_name,
+                "last_name": self.request.user.last_name,
+            }
+        return super().get_initial()
+
+    def get_form(self, form_class=None):
+        """
+        Returns an instance of the form to be used in this view.
+        Checks if the user is authenticated and removes the 'is_create_profile' field.
+        """
+        form = super().get_form(form_class)
+        if self.request.user.is_authenticated:
+            del form.fields["is_create_profile"]
+        return form
 
 
 class OrderViewMixin(BaseView, LoginRequiredMixin):
