8d855a26ffcc8c293a4978c4efe7d9003906d7de
Nikita <nikita.gubarev31@gmail.com>
2023-05-02 22:01:57 +0300

    FEAT: Add an order service function to get or create user

--- lapzone/order/services.py
+++ lapzone/order/services.py
@@ -5,10 +5,13 @@ from django.conf import settings
 from django.contrib import messages
 from django.db.models import QuerySet
 from django.core.mail import send_mail
+from django.contrib.auth.models import User
 from django.http import Http404, HttpRequest
+from allauth.account.models import EmailAddress
 from django.template.loader import render_to_string
 
 from .models import Order
+from .forms import OrderCreateModelForm
 
 
 def send_email_to_customer_by_(
@@ -30,6 +33,23 @@ def send_email_to_customer_by_(
             request=request,
         ),
     )
+
+
+def get_or_create_user_with_data_from_(
+    form: OrderCreateModelForm,
+) -> tuple[User, bool]:
+    """Returns a user with the data from the given OrderCreateModelForm."""
+    user, was_created = User.objects.get_or_create(
+        email=form.cleaned_data["email"]
+    )
+    if was_created:
+        user.username = form.cleaned_data["username"]
+        user.set_password(form.cleaned_data["password"])
+        EmailAddress.objects.create(user=user, email=user.email)
+    user.first_name = form.cleaned_data["first_name"]
+    user.last_name = form.cleaned_data["last_name"]
+    user.save()
+    return user, was_created
 
 
 def get_user_order_from_(queryset: QuerySet, pk: UUID) -> Order | NoReturn:
