5b871f1e31f8e89998497b9c293f4f8bcd529840
Nikita <nikita.gubarev31@gmail.com>
2023-05-10 21:43:10 +0300

    STYLE: Change the message of the js/remove_btn.js
    Replate "Do you really want to delete this item?" with "Do you really want to delete this one?"

--- lapzone/static/js/remove_btn.js
+++ lapzone/static/js/remove_btn.js
@@ -1,7 +1,7 @@
 document.querySelectorAll(".remove.btn").forEach((remove_btn) => {
 	remove_btn.addEventListener("click", function (event) {
 		if (
-			!confirm("Do you really want to delete this item?")
+			!confirm("Do you really want to delete this one?")
 		) event.preventDefault();
 	});
 });
