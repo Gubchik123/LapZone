dcfe6981b06994a80a524b43fd065a96c2e243a0
Nikita <nikita.gubarev31@gmail.com>
2023-05-10 20:48:44 +0300

    FEAT: Create a CustomerDetailView for the customer detail page

--- lapzone/customer/views.py
+++ lapzone/customer/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from typing import Any
+from django.views import generic
 
-# Create your views here.
+from general.views import BaseView
+from .forms import UserModelForm
+
+
+class CustomerDetailView(BaseView, generic.TemplateView):
+    """View for the customer detail page."""
+
+    template_name = "customer/detail.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        """Adds the UserModelForm to the context and returns it."""
+        context = super().get_context_data(**kwargs)
+        context["form"] = UserModelForm(instance=self.request.user)
+        return context
