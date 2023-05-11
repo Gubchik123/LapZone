68b84cdf408ea8fc9e89c831c13d6b7da0421d50
Nikita <nikita.gubarev31@gmail.com>
2023-05-11 21:22:55 +0300

    FEAT: Add url for the CustomerWishListView: "wish-list/"
    Add this url for the "Wish list" list-group-item in the customer/detail.html

--- lapzone/customer/urls.py
+++ lapzone/customer/urls.py
@@ -6,6 +6,7 @@ from . import views
 app_name = "customer"
 urlpatterns = [
     path("", views.CustomerDetailView.as_view(), name="detail"),
+    path("wish-list/", views.CustomerWishListView.as_view(), name="wish_list"),
     path("update/", views.CustomerUpdateView.as_view(), name="update"),
     path("delete/", views.CustomerDeleteView.as_view(), name="delete"),
 ]
