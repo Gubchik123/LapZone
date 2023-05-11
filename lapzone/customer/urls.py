467f84fb1dcbab1d7b1a1430206aeb215792232e
Nikita <nikita.gubarev31@gmail.com>
2023-05-11 20:10:09 +0300

    FEAT: Add url for the CustomerDeleteView: "delete/"
    Add this url for the "Delete my account" button in the customer/detail.html

--- lapzone/customer/urls.py
+++ lapzone/customer/urls.py
@@ -7,4 +7,5 @@ app_name = "customer"
 urlpatterns = [
     path("", views.CustomerDetailView.as_view(), name="detail"),
     path("update/", views.CustomerUpdateView.as_view(), name="update"),
+    path("delete/", views.CustomerDeleteView.as_view(), name="delete"),
 ]
