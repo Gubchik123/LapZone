c756c5d1c216e49502f789ef8c1ca603d1ca1aef
Nikita <nikita.gubarev31@gmail.com>
2023-05-11 19:59:45 +0300

    FEAT: Add a CustomerUpdateView
    to handle the UserModelForm and update the customer data

--- lapzone/customer/views.py
+++ lapzone/customer/views.py
@@ -1,6 +1,11 @@
 from typing import Any
 from django.views import generic
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.db.models.query import QuerySet
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse, HttpResponseRedirect
 
 from general.views import BaseView
 from .forms import UserModelForm
@@ -16,3 +21,29 @@ class CustomerDetailView(BaseView, LoginRequiredMixin, generic.TemplateView):
         context = super().get_context_data(**kwargs)
         context["form"] = UserModelForm(instance=self.request.user)
         return context
+
+
+class CustomerUpdateView(BaseView, generic.UpdateView):
+    """View to handle the UserModelForm and update the customer data."""
+
+    model = User
+    form_class = UserModelForm
+    http_method_names = ["post"]
+    success_url = reverse_lazy("customer:detail")
+
+    def get_object(self, queryset: QuerySet[User] | None = ...) -> User:
+        """Returns the current user."""
+        return self.request.user
+
+    def form_valid(self, form: UserModelForm) -> HttpResponse:
+        """Adds a success message and returns the super method."""
+        messages.success(
+            self.request, "Your personal data has successfully updated."
+        )
+        return super().form_valid(form)
+
+    def form_invalid(self, form: UserModelForm) -> HttpResponse:
+        """Adds form error messages and returns redirect to the success_url."""
+        for error in form.errors.as_data().values():
+            messages.error(self.request, error[0].messages[0])
+        return HttpResponseRedirect(self.success_url)
