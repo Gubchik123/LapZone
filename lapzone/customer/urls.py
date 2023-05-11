a17bff82576f56d99a7fd27ae4aa95ddcbeed976
Nikita <nikita.gubarev31@gmail.com>
2023-05-11 20:00:37 +0300

    FEAT: Add url for the CustomerUpdateView: "update/"
    Add this url for the form in the customer/detail.html

--- lapzone/customer/urls.py
+++ lapzone/customer/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+
 app_name = "customer"
 urlpatterns = [
     path("", views.CustomerDetailView.as_view(), name="detail"),
+    path("update/", views.CustomerUpdateView.as_view(), name="update"),
 ]
