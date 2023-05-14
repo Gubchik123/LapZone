a43d83978806407b808450e6e0996345254fce53
Nikita <nikita.gubarev31@gmail.com>
2023-05-14 22:06:04 +0300

    FEAT: Make the submit_loader.js more versatile

--- lapzone/static/js/order/submit_loader.js
+++ lapzone/static/js/order/submit_loader.js
@@ -1,15 +1,14 @@
 const screen_loader = document.getElementById("screen-loader");
-const checkout_form = document.getElementById("checkout-form");
-const checkout_submit_btn = document.getElementById("checkout-submit-btn");
+const form = document.querySelector("form.loading");
 
 function is_all_required_fields_are_filled() {
-	for (const input of checkout_form.querySelectorAll("input"))
+	for (const input of form.querySelectorAll("input"))
 		if (input.hasAttribute("required") && input.value.trim() === "")
 			return false;
 	return true;
 }
 
-checkout_submit_btn.addEventListener("click", function (event) {
+document.querySelector(".loading.btn").addEventListener("click", function (event) {
 	if (is_all_required_fields_are_filled()) {
 		screen_loader.style.display = "block";
 		screen_loader.classList.remove("d-none");
