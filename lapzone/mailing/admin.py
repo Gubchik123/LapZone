dfe5c8d06640d90c9850e4ff20f41f0baab4ee78
Nikita <nikita.gubarev31@gmail.com>
2023-05-14 20:05:10 +0300

    FEAT: Create the "Mailing" app model admin class
    MailingEmailAddressModelAdmin

--- lapzone/mailing/admin.py
+++ lapzone/mailing/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import MailingEmailAddress
+
+
+@admin.register(MailingEmailAddress)
+class MailingEmailAddressModelAdmin(admin.ModelAdmin):
+    """Admin class for the managing MailingEmailAddress instances."""
+
+    list_filter = ["created"]
+    search_fields = ["email"]
+    readonly_fields = ["id", "created"]
+    search_help_text = "Searching by email address."
+    list_display = fields = ["id", "email", "created"]
