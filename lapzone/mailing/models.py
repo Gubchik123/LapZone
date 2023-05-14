a3fb725aa0ec0b107d808ffc13136c1da0517f87
Nikita <nikita.gubarev31@gmail.com>
2023-05-14 19:49:04 +0300

    FEAT: Create the "Mailing" app model: MailingEmailAddress

--- lapzone/mailing/models.py
+++ lapzone/mailing/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from general.models import ModelWithUUIDPK, ModelWithCreatedDateTime
+
+
+class MailingEmailAddress(
+    ModelWithUUIDPK, ModelWithCreatedDateTime, models.Model
+):
+    """
+    A model representing an email address for mailing.
+    Fields: id (uuid4), created, email.
+    """
+
+    email = models.EmailField(
+        "Email address", unique=True, blank=False, null=False
+    )
+
+    class Meta:
+        ordering = ["-created"]
+        verbose_name = "Mailing email address"
+        verbose_name_plural = "Mailing email addresses"
