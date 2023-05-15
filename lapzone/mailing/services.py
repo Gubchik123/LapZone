db2e839e44daed724f87fb90fb71e91a33ef19df
Nikita <nikita.gubarev31@gmail.com>
2023-05-15 19:45:27 +0300

    FEAT: Improve the mailing service funtion for sending mail
    Replace the 'email' funciton parameter with mailing_email_address: MailingEmailAddress and request: HttpRequest
    Add pk and request in the context of 'render_to_string' function

--- lapzone/mailing/services.py
+++ lapzone/mailing/services.py
@@ -1,15 +1,25 @@
 from django.conf import settings
+from django.http import HttpRequest
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 
+from .models import MailingEmailAddress
 
-def send_mail_to_(email: str) -> None:
+
+def send_mail_to_(mailing_email_address: MailingEmailAddress, request: HttpRequest) -> None:
     """Sends an email to the given email address."""
     subject_prefix = settings.ACCOUNT_EMAIL_SUBJECT_PREFIX
     send_mail(
         f"{subject_prefix}You have successfully subscribed to our mailing",
         "You will be noticed about all our changes.",
         settings.EMAIL_HOST_USER,
-        [email],
-        html_message=render_to_string("mailing/email.html", {"email": email}),
+        [mailing_email_address.email],
+        html_message=render_to_string(
+            "mailing/email.html",
+            {
+                "pk": mailing_email_address.pk,
+                "email": mailing_email_address.email,
+            },
+            request,
+        ),
     )
