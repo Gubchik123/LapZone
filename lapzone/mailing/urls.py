d938cfc5e94de6329fdcf5a29c59f9a03bd2d2e6
Nikita <nikita.gubarev31@gmail.com>
2023-05-15 19:43:42 +0300

    FEAT: Add an url for the MailingDeleteView: "<uuid:pk>/delete/"

--- lapzone/mailing/urls.py
+++ lapzone/mailing/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 app_name = "mailing"
 urlpatterns = [
-    path("create/", views.MailingCreateView.as_view(), name="create")
+    path("create/", views.MailingCreateView.as_view(), name="create"),
+    path(
+        "<uuid:pk>/delete/", views.MailingDeleteView.as_view(), name="delete"
+    ),
 ]
