5d9e3eb7c968958c038f2d525b747810d65732ca
Nikita <nikita.gubarev31@gmail.com>
2023-05-26 16:13:56 +0300

    SETTINGS: Update static settings
    Uncomment the STATIC_ROOT
    Remove the STATIC_DIR and STATICFILES_DIRS

--- lapzone/lapzone/settings.py
+++ lapzone/lapzone/settings.py
@@ -110,9 +110,7 @@ USE_I18N = False
 USE_TZ = True
 
 STATIC_URL = "static/"
-STATIC_DIR = BASE_DIR / "static"
-STATICFILES_DIRS = [STATIC_DIR]
-# STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "static"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
