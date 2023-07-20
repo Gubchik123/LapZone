492311099e9ba06fec336db3e3fa487a7029f800
Nikita <nikita.gubarev31@gmail.com>
2023-07-20 17:10:22 +0300

    FEAT: Add more special content to stop bots from crawling

--- lapzone/views.py
+++ lapzone/views.py
@@ -3,6 +3,22 @@ from django.http import HttpRequest, HttpResponse
 
 def get_robots_txt(request: HttpRequest) -> HttpResponse:
     """Returns special content to stop bots from crawling my site."""
-    return HttpResponse(
-        "User-agent: SemrushBot\nDisallow: /", content_type="text/plain"
-    )
+    content = """
+        User-agent: SemrushBot
+        Disallow: /
+        User-agent: SiteAuditBot
+        Disallow: /
+        User-agent: SemrushBot-BA
+        Disallow: /
+        User-agent: SemrushBot-SI
+        Disallow: /
+        User-agent: SemrushBot-SWA
+        Disallow: /
+        User-agent: SemrushBot-CT
+        Disallow: /
+        User-agent: SplitSignalBot
+        Disallow: /
+        User-agent: SemrushBot-COUB
+        Disallow: /
+    """.replace("        ", "").strip()
+    return HttpResponse(content, content_type="text/plain")
