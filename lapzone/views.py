1856858c5e20d692c80770ff10063b29a70088a4
Nikita <nikita.gubarev31@gmail.com>
2023-08-26 14:27:58 +0300

    FEAT: Use all user-agents to stop bots from crawling

--- lapzone/views.py
+++ lapzone/views.py
@@ -4,21 +4,7 @@ from django.http import HttpRequest, HttpResponse
 def get_robots_txt(request: HttpRequest) -> HttpResponse:
     """Returns special content to stop bots from crawling my site."""
     content = """
-        User-agent: SemrushBot
-        Disallow: /
-        User-agent: SiteAuditBot
-        Disallow: /
-        User-agent: SemrushBot-BA
-        Disallow: /
-        User-agent: SemrushBot-SI
-        Disallow: /
-        User-agent: SemrushBot-SWA
-        Disallow: /
-        User-agent: SemrushBot-CT
-        Disallow: /
-        User-agent: SplitSignalBot
-        Disallow: /
-        User-agent: SemrushBot-COUB
+        User-agent: *
         Disallow: /
     """.replace("        ", "").strip()
     return HttpResponse(content, content_type="text/plain")
