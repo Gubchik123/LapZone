e1af887b431d6cef6da7c25a15e610d64ac73947
Nikita Hubariev <101975871+Gubchik123@users.noreply.github.com>
2024-04-02 13:21:56 +0300

    Add checking on gmail email in the mailing form

--- mailing/forms.py
+++ mailing/forms.py
@@ -6,6 +6,14 @@ from .models import MailingEmailAddress
 class MailingEmailAddressModelForm(forms.ModelForm):
     """Form for creating a mailing email address."""
 
+    def clean_email(self):
+        """Check if email is valid."""
+        email = self.cleaned_data.get("email")
+
+        if not email.endswith("@gmail.com"):
+            raise forms.ValidationError("Please enter a Gmail email address.")
+        return email
+
     class Meta:
         """Meta options for the MailingEmailAddressModelForm."""
 
@@ -15,4 +23,5 @@ class MailingEmailAddressModelForm(forms.ModelForm):
             "email": forms.EmailInput(
                 attrs={"class": "form-control", "placeholder": "Email address"}
             )
-        }
+        }   
+ 
